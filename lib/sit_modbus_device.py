c28d3ec3b6434e2d1e06c135a617d9480fa3d743
phgachoud <ph.gachoud@gmail.com>
2020-05-26 05:01:23 +0000

    added_license_and_comments

--- lib/sit_modbus_device.py
+++ lib/sit_modbus_device.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python3
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 #       DESCRIPTION: 
-#			Library for mobdbus operations
-#				sys.path.append('../lib/')
-#       CALL SAMPLE:
-#                
-#       CALL PARAMETERS:
-#               1) 
+#			Library for mobdbus operations, see cluster_controller.py for example of use
+#
 #	REQUIRE
 #		**** PYTHON *****
 #		sudo apt-get install python-pygments python-pip python-pymodbus python3-pip
@@ -16,8 +12,30 @@
 #		sudo apt install python3-pip
 #		sudo pip3 install requests click pymodbus prompt_toolkit
 #
+#		*************************************************************************************************
 #       @author: Philippe Gachoud
 #       @creation: 20200402
+#       @last modification:
+#       @version: 1.0
+#       @URL: $URL
+#		*************************************************************************************************
+#		Copyright (C) 2020 Solarity spa
+
+#		This library is free software; you can redistribute it and/or
+#		modify it under the terms of the GNU Lesser General Public
+#		License as published by the Free Software Foundation; either
+#		version 2.1 of the License, or (at your option) any later version.
+#
+#		This library is distributed in the hope that it will be useful,
+#		but WITHOUT ANY WARRANTY; without even the implied warranty of
+#		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#		Lesser General Public License for more details.
+#
+#		You should have received a copy of the GNU Lesser General Public
+#		License along with this library; if not, write to the Free Software
+#		Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#		*************************************************************************************************
+#
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 # INCLUDES
